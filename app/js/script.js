598ce5028a9e0a97a9091d154fabb009b69e8dd6
Nicolas Oliveira <nicolsoliverg@gmail.com>
2022-09-15 19:21:34 -0300

    JS: setup (pretty) basic application structure

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,30 @@
-console.log('Hello World');
+// JSON diretory
+const URL = "../../data.json";
+
+// Timer HTML Elements
+const timerElement = document.getElementsByClassName('timer__inside');
+
+function createTimer() {
+    timerElement[0].innerHTML = `
+        <div>
+          <label>Work</label>
+          <a class="timer__inside--button">
+            <span></span>
+            <span></span>
+            <span></span>
+          </a>
+        </div>
+        <div>
+          <span>32hrs</span>
+          <span>Last Week - 36hrs</span>
+        </div>
+    `
+}
+
+createTimer();
+
+fetch(URL)
+    .then(( resp ) => resp.json())
+    .then(( json ) => {
+        console.log(json[0].timeframes.daily);
+    });
